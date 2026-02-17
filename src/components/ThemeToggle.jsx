c7f9daae15a99d437b4a83a50e6c40f9b76d0ae3
Aaryan <aaryangautam291@gmail.com>
2026-02-17 23:35:53 +0530

    Initial portfolio setup

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import { Moon, Sun } from "lucide-react";
+
+export const ThemeToggle = () => {
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () =>{
+        if(isDarkMode){
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme","light");
+            setIsDarkMode(false);
+        }
+        else{
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme","dark");
+            setIsDarkMode(true);
+        }
+    };
+
+    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}</button>
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggle'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+      {/*Theme Toggle */}
+      <ThemeToggle/>
+      {/*BAckground Effect */}
+
+      {/*Navbar */}
+
+      {/*Main Content */}
+
+      {/*Footer */}
+    </div>
+  )
+}
+
+export default Home
