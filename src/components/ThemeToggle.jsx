3fcb41777ed22878a3fec3c47bfc2c0ec487f81b
Aaryan <aaryangautam291@gmail.com>
2026-02-18 20:47:03 +0530

    Completed Starbackground

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react"
 import { Moon, Sun } from "lucide-react";
+import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
 
     const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(()=>{
+        const storedTheme = localStorage.getItem("theme")
+        if(storedTheme==="dark"){
+            setIsDarkMode(true)
+            document.documentElement.classList.add("dark");
+        }
+        else {
+            localStorage.setItem("theme","light");
+            setIsDarkMode(false);
+        }
+    }, [])
 
     const toggleTheme = () =>{
         if(isDarkMode){
@@ -18,5 +31,7 @@ export const ThemeToggle = () => {
         }
     };
 
-    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}</button>
+    return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", 
+        "focus:outin-hidden"
+    )}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}</button>
 }
