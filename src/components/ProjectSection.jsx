b8341c66b8d0ccd3cab7d8e9b0ee80a2a4a5a12b
Aaryan <aaryangautam291@gmail.com>
2026-02-24 23:27:37 +0530

    almost done

---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,79 @@
+import { ArrowRight, ExternalLinkIcon, GithubIcon } from "lucide-react";
+
+
+const projects = [
+    {
+        id: 1,
+        title: "React Job Protal",
+        description: "Job Portal For React devs",
+        image: "/projects/project1.png",
+        tags: ["React", "Tailwind"],
+        demoUrl: "#",
+        githubUrl: "https://github.com/aaryangautam-dot/Myprojects.git",
+    },
+
+        {
+        id: 2,
+        title: "Text to Image Generator",
+        description: "A responsive Website which converts text to image using ai",
+        image: "/projects/project2.png",
+        tags: ["MERN", "React", "Tailwind"],
+        demoUrl: "#",
+        githubUrl: "https://github.com/aaryangautam-dot/Myprojects.git",
+    },
+];
+
+export const ProjectSection = () => {
+    return (
+        <section id ="projects" className="py-24 px-4 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+                    Featured <span className="text-primary"> Projects</span>
+                </h2>
+
+
+                <p className="text-center text-muteed-foreground mb-12 max-w-2xl mx-auto">
+
+                     Here are some of my recent projects. Each project was carefully 
+                     crafted with attention to detail, performance, and user experience</p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {projects.map((projects,key)=> (
+                        <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                            <div className="h-48 overflow-hidden">
+                            <img src={projects.image} alt={projects.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                            </div>
+
+                            <div className="p-6 ">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {projects.tags.map((tag)=>(
+                                        <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">{tag}</span>
+                                    ))}
+                                </div>
+
+                                <h3 className="text-xl font-semibold mb-1">{projects.title}</h3>
+                                <p className="text-muted-foreground text-sm mb-4">{projects.description}</p>
+
+                                <div className="flex jusitfy-between items-center">
+                                   <div className="flex - space-x-3">
+                                    <a href={projects.demoUrl}
+                                    target="_blank" className="text-foreground/80 hover:text-primary transiton-colors duration-300"> <ExternalLinkIcon size={20}/></a>
+
+                                    <a href={projects.githubUrl}
+                                    target="_blank" className="text-foreground/80 hover:text-primary transiton-colors duration-300"><GithubIcon size={20}/></a>
+                                    </div> 
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="text-center mt-12">
+                    <a className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                    target="_blank" 
+                    href="https://github.com/aaryangautam-dot" >Check My Github <ArrowRight size={16}/></a>
+                </div>
+            </div>
+        </section>
+    );
+};
